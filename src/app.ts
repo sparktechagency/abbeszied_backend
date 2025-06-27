2123aec3f767126cb84a9802ee8002ba52160944
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-06-27 18:13:23 +0600

    restore code

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }),
 );
 
